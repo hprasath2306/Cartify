1c6b7973e697154927a0d01570fcad0a8d710854
hprasath2306 <hariprasaths21cs@psnacet.edu.in>
2024-11-23 12:42:16 +0530

    Small changes

--- api/src/index.ts
+++ api/src/index.ts
@@ -14,9 +14,6 @@ app.get("/", (req, res) => {
   res.send("Server is Ready!!");
 });
 
-if (process.env.NODE_ENV === "dev") {
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
-}
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
